502d95042befeb3ff1a6c0f7cb6206a515e07dd7
maulanazn <starknntnk99@gmail.com>
2023-12-28 10:33:41 +0700

    Repositioning board, green constraint line, ruler line, and text.

--- js/layup-drawer.js
+++ js/layup-drawer.js
@@ -23,16 +23,16 @@ LayupDrawer.prototype = {
         
         for (let i = 0; i < 15; i++) {
             ctx.beginPath()
-            ctx.moveTo(130, i * 40.7)
-            ctx.lineTo(150, i * 40.7)
+            ctx.moveTo(130, i * 35.7)
+            ctx.lineTo(150, i * 35.7)
             ctx.strokeStyle = "gray"
             ctx.stroke();
         }
 
-        for (let i = 0; i < 9; i++) {
+        for (let i = 0; i < 8; i++) {
             ctx.beginPath()
-            ctx.moveTo(i * 150, 590)
-            ctx.lineTo(i * 150, 570)
+            ctx.moveTo(i * 150, 520)
+            ctx.lineTo(i * 150, 500)
             ctx.strokeStyle = "gray"
             ctx.stroke();
         }
@@ -62,7 +62,7 @@ LayupDrawer.prototype = {
         ctx.shadowOffsetX = 10
         ctx.shadowOffsetY = 10
         ctx.shadowBlur = 10
-        ctx.strokeRect(150, 40, 1080, 530);
+        ctx.strokeRect(150, 40, 980, 460);
     },
 
     drawBigCanvas: function(ctx) {
@@ -77,40 +77,40 @@ LayupDrawer.prototype = {
     drawGreenLineHorizontal: function(ctx) {
         for (let j = 0; j < 5; j++) {
             ctx.beginPath()
-            ctx.moveTo(150, j * 119.9);
-            ctx.lineTo(1230, j * 119.9)
+            ctx.moveTo(150, j * 105.9);
+            ctx.lineTo(1130, j * 105.9)
             ctx.strokeStyle = "#51F728"
+
+
             ctx.stroke()
         }
     },
 
     drawBoard: function(ctx, object1, object2) {
         object1.addEventListener("load", (e) => {
-            ctx.drawImage(object1, 400, 57, 75, 107, 150, 40, 1080, 100)
-            ctx.drawImage(object1, 400, 67, 75, 107, 150, 240, 1080, 120)
-            ctx.drawImage(object1, 400, 67, 75, 107, 150, 470, 1080, 100)
+            ctx.drawImage(object1, 400, 57, 75, 107, 150, 40, 980, 90)
+            ctx.drawImage(object1, 400, 67, 75, 107, 150, 220, 980, 90)
+            ctx.drawImage(object1, 400, 67, 75, 107, 150, 410, 980, 90)
             this.drawGreenLineHorizontal(ctx)
         })
         object1.src = "../images/paralel-grain-0.jpg"
         
         object2.addEventListener('load', () => {
             // Upper second board
-            ctx.drawImage(object2, 150, 140, 180, 100)
-            ctx.drawImage(object2, 320, 140, 180, 100)
-            ctx.drawImage(object2, 490, 140, 180, 100)
-            ctx.drawImage(object2, 660, 140, 180, 100)
-            ctx.drawImage(object2, 830, 140, 180, 100)
-            ctx.drawImage(object2, 1000, 140, 180, 100)
-            ctx.drawImage(object2, 1050, 140, 180, 100)
+            ctx.drawImage(object2, 150, 130, 180, 90)
+            ctx.drawImage(object2, 320, 130, 180, 90)
+            ctx.drawImage(object2, 490, 130, 180, 90)
+            ctx.drawImage(object2, 660, 130, 180, 90)
+            ctx.drawImage(object2, 830, 130, 180, 90)
+            ctx.drawImage(object2, 1000, 130, 130, 90)
 
             // Bottom second board
-            ctx.drawImage(object2, 150, 360, 180, 110)
-            ctx.drawImage(object2, 320, 360, 180, 110)
-            ctx.drawImage(object2, 490, 360, 180, 110)
-            ctx.drawImage(object2, 660, 360, 180, 110)
-            ctx.drawImage(object2, 830, 360, 180, 110)
-            ctx.drawImage(object2, 1000, 360, 180, 110)
-            ctx.drawImage(object2, 1050, 360, 180, 110)
+            ctx.drawImage(object2, 150, 310, 180, 100)
+            ctx.drawImage(object2, 320, 310, 180, 100)
+            ctx.drawImage(object2, 490, 310, 180, 100)
+            ctx.drawImage(object2, 660, 310, 180, 100)
+            ctx.drawImage(object2, 830, 310, 180, 100)
+            ctx.drawImage(object2, 1000, 310, 130, 100)
             this.drawGreenLineHorizontal(ctx)
         })
         object2.src = "../images/perpendicular-grain-90.jpg"
@@ -118,26 +118,28 @@ LayupDrawer.prototype = {
 
     drawNumberText: function(ctx) {
         ctx.font = "20px Noto Sans"
-        ctx.fillText(180, 90, 88);
-        ctx.fillText(120, 90, 250);
-        ctx.fillText(60, 100, 415);
-        ctx.fillText(0, 110, 575);
+        // Y Axis
+        ctx.fillText(180, 90, 80);
+        ctx.fillText(120, 90, 220);
+        ctx.fillText(60, 100, 360);
+        ctx.fillText(0, 110, 505);
 
-        ctx.fillText(150, 1183, 608);
-        ctx.fillText(120, 1033, 608);
-        ctx.fillText(90, 740, 608);
-        ctx.fillText(60, 590, 608);
-        ctx.fillText(30, 290, 608);
-        ctx.fillText(0, 143, 608);
+        // X Axis
+        ctx.fillText(150, 1030, 540);
+        ctx.fillText(120, 880, 540);
+        ctx.fillText(90, 740, 540);
+        ctx.fillText(60, 590, 540);
+        ctx.fillText(30, 440, 540);
+        ctx.fillText(0, 143, 540);
     },
 
     drawBoardInfo: function(ctx, layup) {
-        ctx.font = "10px Noto Sans"
-        ctx.fillText(`${layup.t1.label.toLowerCase()}: ${layup.t1.thickness} ${layup.t1.grade}`, 1250, 80)
-        ctx.fillText(`${layup.t2.label.toLowerCase()}: ${layup.t2.thickness} ${layup.t2.grade}`, 1250, 180)
-        ctx.fillText(`${layup.t3.label.toLowerCase()}: ${layup.t3.thickness} ${layup.t3.grade}`, 1250, 280)
-        ctx.fillText(`${layup.t4.label.toLowerCase()}: ${layup.t4.thickness} ${layup.t4.grade}`, 1250, 410)
-        ctx.fillText(`${layup.t5.label.toLowerCase()}: ${layup.t5.thickness} ${layup.t5.grade}`, 1250, 520)
+        ctx.font = "15px Noto Sans"
+        ctx.fillText(`${layup.t1.label.toLowerCase()}: ${layup.t1.thickness} ${layup.t1.grade}`, 1150, 80)
+        ctx.fillText(`${layup.t2.label.toLowerCase()}: ${layup.t2.thickness} ${layup.t2.grade}`, 1150, 180)
+        ctx.fillText(`${layup.t3.label.toLowerCase()}: ${layup.t3.thickness} ${layup.t3.grade}`, 1150, 280)
+        ctx.fillText(`${layup.t4.label.toLowerCase()}: ${layup.t4.thickness} ${layup.t4.grade}`, 1150, 380)
+        ctx.fillText(`${layup.t5.label.toLowerCase()}: ${layup.t5.thickness} ${layup.t5.grade}`, 1150, 460)
     },
 
     drawExtraInfo: function(ctx) {
@@ -149,7 +151,7 @@ LayupDrawer.prototype = {
 
         ctx.save()
         ctx.font = "20px Noto Sans"
-        ctx.fillText("Primary Direction", 580, 635)
+        ctx.fillText("Primary Direction", 580, 605)
         ctx.restore()
     }
 };
